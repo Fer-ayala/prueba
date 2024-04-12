abb54fc8959cd543f7077d321a19536e5ba02eba
Fernando <monasoxx@gmail.com>
2024-04-12 01:20:40 -0500

    cambios

---
+++ Alumno.java
@@ -0,0 +1,25 @@
+public class Alumno {
+    private int edad;
+    private int nota;
+
+    public Alumno(int edad, int nota){
+        this.edad = edad;
+        this.nota = nota;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+}
